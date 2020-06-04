59c33beb8e979b552b674f36e410d252120166dd
Slawomir Bialynicki-Birulo <slawomirbialynicki-birulo@Slawomirs-MacBook-Pro.local>
2020-06-04 02:00:13 +0200

    hamburger animation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,59 @@
-import React from 'react';
-import './Footer.scss'
+import './Footer.scss';
+import React, { Component } from 'react';
 
-function Footer() {
-    return (
-      <nav className='nav-bar'>
-          <ol className='nav-list'>
-              <li><a href='/#about'>ABOUT ME</a></li>
-              <li><a href='/#projects'>PROJECTS!</a></li>
-              <li><a href='/#education'>EDUCATION</a></li>
-          </ol>
-          <div className='menu'>
-            <div className='lines'></div>
-            <div className='lines'></div>
-            <div className='lines'></div>
-          </div>
-      </nav>
-    );
+
+class Footer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      sideBarOut: false,
+      sidebarClassLineOne: 'line1',
+      sidebarClassLineTwo: 'line2',
+      sidebarClassLineThree: 'line3'
+    }
   }
 
-  export default Footer;
+  hamburgerToggle = () => {
+     if(!this.state.sideBarOut) {
+       this.setState({
+        sidebarClassLineOne: 'line1out',
+        sidebarClassLineTwo: 'line2out',
+        sidebarClassLineThree: 'line3out',
+        sideBarOut: true,
+       })
+     }
+     else{
+      this.setState({
+        sidebarClassLineOne: 'line1',
+        sidebarClassLineTwo: 'line2',
+        sidebarClassLineThree: 'line3',
+        sideBarOut: false,
+       })
+     }
+  }
+
+
+
+
+  render() {
+    return (
+      <nav className='nav-bar'>
+        <ol className='nav-list'>
+          <li><a href='/#about'>ABOUT ME</a></li>
+          <li><a href='/#projects'>PROJECTS</a></li>
+          <li><a href='/#education'>EDUCATION</a></li>
+        </ol>
+        <div onClick = {this.hamburgerToggle} className='menu'>
+          <div className={this.state.sidebarClassLineOne}></div>
+          <div className={this.state.sidebarClassLineTwo}></div>
+          <div className={this.state.sidebarClassLineThree}></div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+
+
+export default Footer;
