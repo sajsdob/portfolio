30aa99645c4c6e8b4ab9d7baafc178b12f777e3b
Slawomir Bialynicki-Birulo <slawomirbialynicki-birulo@Slawomirs-MacBook-Pro.local>
2020-06-07 21:30:10 +0200

    line

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './About.scss';
+import face from '../../assets/logo.png'
 
 
 class About extends Component {
@@ -19,13 +20,15 @@ class About extends Component {
                 <h1>HELLO, MY NAME IS</h1>
                 <h2>Slawomir</h2>
                 <h2>Bialynicki - Birulo.</h2>
-                <h3>-----------------------------------------------</h3>
+                {/* <h3>-----------------------------------------------</h3> */}
+                <div className='line'></div>
                 <div>
                     <p>I'm an aspiring front-end developer based in Poznań, POLAND. I'm specializing in building websites, web applications and everything in between.</p>
                 </div>
                 <div className='contact-me'>
                     <a href='mailto: sajsdob@gmail.con'>REACH OUT</a>
                 </div>
+                <img src={face}></img>
             </div>
         )
     }
