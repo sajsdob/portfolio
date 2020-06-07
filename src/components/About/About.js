1bc5c2502d9b446bbe668bf2cd1fc3642cae3648
Slawomir Bialynicki-Birulo <slawomirbialynicki-birulo@Slawomirs-MacBook-Pro.local>
2020-06-07 15:03:55 +0200

    restyle

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,9 +19,9 @@ class About extends Component {
                 <h1>HELLO, MY NAME IS</h1>
                 <h2>Slawomir</h2>
                 <h2>Bialynicki - Birulo.</h2>
-                <h3>I'm an aspiring front-end developer.</h3>
+                <h3>-----------------------------------------------</h3>
                 <div>
-                    <p>I'm a future developer based in Poznań, POLAND. I'm specializing in building websites and everything in between.</p>
+                    <p>I'm an aspiring front-end developer based in Poznań, POLAND. I'm specializing in building websites, web applications and everything in between.</p>
                 </div>
                 <div className='contact-me'>
                     <a href='mailto: sajsdob@gmail.con'>REACH OUT</a>
