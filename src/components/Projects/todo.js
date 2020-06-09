11c64f65f609b31d426c66818f914a114ab3b882
Slawomir Bialynicki-Birulo <slawomirbialynicki-birulo@Slawomirs-MacBook-Pro.local>
2020-06-10 01:20:53 +0200

    title

--- src/components/Projects/todo.js
+++ src/components/Projects/todo.js
@@ -13,7 +13,7 @@ constructor(props){
     const {items, deleted, addToDone} = this.props;
     return(
       <ul>
-        <h1 className = 'todo'>TO DO</h1>
+        <h1 className = 'todo'>TO DO:</h1>
         {items.map((item, index)=><li className = 'nie' onClick={(e)=>deleted(index)} key={index}>{item} <br/> <button className='tak' onClick={(e)=>addToDone(item)}> MOVE TO DONE</button> </li>)}
       </ul>
     )
