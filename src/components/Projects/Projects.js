75a47c64112190c173ce994a6ad58c1bab3cb2e2
Slawomir Bialynicki-Birulo <slawomirbialynicki-birulo@Slawomirs-MacBook-Pro.local>
2020-06-14 02:02:35 +0200

    resap

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,7 +10,7 @@ AOS.init({ duration: 800 });
 
 function Projects() {
     return <div id='projects'>
-        <p className='myprojects'>HERE ARE SOME THINGS I'VE BEEN WORKING ON:</p>
+        <h1 className='myprojects'>HERE ARE SOME THINGS I'VE BEEN WORKING ON:</h1>
         <div className='project-list'>
             <Link to="/Todoapp">
                 <div data-aos="fade-up" className='todo-list'><div className='background-color'></div><h1>TO DO LIST</h1></div>
