9b105dac9afd5b243f9bc1d9211ec84106558b7d
Slawomir Bialynicki-Birulo <slawomirbialynicki-birulo@Slawomirs-MacBook-Pro.local>
2020-06-15 22:58:42 +0200

    heightb

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,24 +2,27 @@ import React from 'react';
 import './Skills.scss';
 import reactlogo from '../../assets/reactlogo.png';
 import sasslogo from '../../assets/sasslogo.png';
-import htmllogo from '../../assets/htmllogo.png'
-import javascriptlogo from '../../assets/javascriptlogo.png'
+import htmllogo from '../../assets/htmllogo.png';
+import javascriptlogo from '../../assets/javascriptlogo.png';
+import csslogo from '../../assets/csslogo.png';
+
 
 
 function Skills() {
-    
+
     // function serachMovies () {
     //     fetch(proxyUrl + targetUrl)
     //     .then(res => res.json())
     //     .then (weather => console.log(weather))
     // }
- 
+
     return <div id='/Skills' className='Skills'>
-                <img alt='reactjslogos' className='react-logo' src={reactlogo}/>
-                <img alt='sasslogo' className='sass-logo' src={sasslogo}/>
-                <img alt='htmllogo' className='htmllogo' src={htmllogo}/>
-                <img alt='javascriptlogo' className='javascriptlogo' src={javascriptlogo}/>
-           </div>
+        <img alt='csslogo' className='csslogo' src={csslogo} />
+        <img alt='reactjslogos' className='react-logo' src={reactlogo} />
+        <img alt='sasslogo' className='sass-logo' src={sasslogo} />
+        <img alt='javascriptlogo' className='javascriptlogo' src={javascriptlogo} />
+        <img alt='htmllogo' className='htmllogo' src={htmllogo} />
+    </div>
 }
 
 
