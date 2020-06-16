ef6477fdefe6f89c6d564ea4e9493839a039b0ed
Slawomir Bialynicki-Birulo <slawomirbialynicki-birulo@Slawomirs-MacBook-Pro.local>
2020-06-17 01:59:41 +0200

    api fixs

--- src/components/Weatherapp/Weatherapp.js
+++ src/components/Weatherapp/Weatherapp.js
@@ -33,7 +33,7 @@ AOS.init({ duration: 800 });
 
 const api = {
     key: "2bfdd7ade4a7269949294a30207a1c19",
-    base: "api.openweathermap.org/data/2.5/",
+    base: "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/",
     proxyUrl: 'https://cors-anywhere.herokuapp.com/'
 }
 
