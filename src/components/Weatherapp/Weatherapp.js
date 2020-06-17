6440db5826046f89960e53bca1a01e31523d5468
Slawomir Bialynicki-Birulo <slawomirbialynicki-birulo@Slawomirs-MacBook-Pro.local>
2020-06-17 12:41:50 +0200

    apikeay

--- src/components/Weatherapp/Weatherapp.js
+++ src/components/Weatherapp/Weatherapp.js
@@ -6,7 +6,7 @@
 
 
 // const  Weatherapp = () => {
-
+    
 //     function searchWeather () {
 //         fetch(proxyUrl + targetUrl)
 //         .then(res => res.json())
@@ -22,78 +22,56 @@
 
 // export default Weatherapp
 
+
+
 import React, { useState } from 'react';
 import './Weatherapp.scss';
-// import AOS from 'aos';
-// import 'aos/dist/aos.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 
 // AOS.init({ duration: 800 });
 
-const api = {
-    key: "3dbbfca50d3674cc4510970916791fb0",
-    base: "https://api.openweathermap.org/data/2.5/",
-    proxyUrl: 'http://cors-anywhere.herokuapp.com/'
-}
+// const api = {
+//     key: "6557810176c36fac5f0db536711a6c52",
+//     base: "https://api.openweathermap.org/data/2.5/",
+//     proxyUrl: 'http://cors-anywhere.herokuapp.com/'
+// }
 
 function Weatherapp() {
 
-    const [weatherinfo, setWeatherinfo] = useState({});
-    const [link, setLink] = useState('');
+    // const [weatherinfo, setWeatherinfo] = useState({});
+    // const [link, setLink] = useState('');
 
 
-    const searchCity = evt => {
-        if (evt.key === "Enter") {
-            fetch(`${api.base}weather?q=${link}&units=metric&APPID=${api.key}`)
-                .then(res => res.json())
-                .then(result => {
-                    setWeatherinfo(result);
-                    setLink('');
-                });
-        }
-    }
+    // const searchCity = evt => {
+    //     if (evt.key === "Enter") {
+    //         fetch(`${api.base}weather?q=${link}&units=metric&APPID=${api.key}`)
+    //             .then(res => res.json())
+    //             .then(result => {
+    //                 setWeatherinfo(result);
+    //                 setLink('');
+    //             });
+    //     }
+    // }
 
-    const currentDate = (d) => {
-        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    // const currentDate = (d) => {
+    //     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    //     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-        let day = days[d.getDay()];
-        let date = d.getDate();
-        let month = months[d.getMonth()];
-        let year = d.getFullYear();
+    //     let day = days[d.getDay()];
+    //     let date = d.getDate();
+    //     let month = months[d.getMonth()];
+    //     let year = d.getFullYear();
 
-        return `${day} ${date} ${month} ${year}`
-    }
+    //     return `${day} ${date} ${month} ${year}`
+    // }
 
     return (
         <div data-aos="slide-down"  className='app'>
-            <main>
-                <div className="search-box">
-                    <input
-                        type="text"
-                        className="search-bar"
-                        placeholder="Search..."
-                        onChange={e => setLink(e.target.value)}
-                        value={link}
-                        onKeyPress={searchCity}
-                    />
-                </div>
-                {(typeof weatherinfo.main != "undefined") ? (
-                    <div>
-                        <div className="location-box">
-                            <div className="location">{weatherinfo.name}, {weatherinfo.sys.country}</div>
-                            <div className="date">{currentDate(new Date())}</div>
-                        </div>
-                        <div className="weather-box">
-                            <div className="temp">
-                                {Math.round(weatherinfo.main.temp)}°c
-            </div>
-                            <div className="weather">{weatherinfo.weather[0].main}</div>
-                        </div>
-                    </div>
-                ) : ('')}
-            </main>
+fkalfmawlkfaw
+
         </div>
     );
 }
