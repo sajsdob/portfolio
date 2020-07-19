317b4b439d0ded662a718fb6a4ca8c752c9bac80
Slawomir Bialynicki-Birulo <slawomirbialynicki-birulo@Slawomirs-MacBook-Pro.local>
2020-07-19 18:09:43 +0200

    transition fix

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './todoapp.scss';
+import './Projects.scss';
 import { Link } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -19,21 +19,21 @@ function Projects() {
 
     return <div id='projects'>
         <h1 className='myprojects'>PROJECTS I'VE BEEN WORKING ON:</h1>
-        <div className='project-list'>
-            <Link className='boxshadow' to="/Todoapp">
-                <div onClick={toTop} data-aos-once='true' data-aos="fade-up" className='todo-list'><div className='background-color'></div><h1>TO DO LIST</h1></div>
+        <div  className='project-list'>
+            <Link data-aos-once='true' data-aos="fade-up" className='boxshadow' to="/Todoapp">
+                <div onClick={toTop}  className='todo-list'><div className='background-color'></div><h1>TO DO LIST</h1></div>
                 <div data-aos-once='true' data-aos="fade-up" className='sepLine'></div>
             </Link>
-            <Link to="/Weatherapp">
-                <div onClick={toTop} data-aos-once='true' data-aos="fade-up" className='todo-list'><div className='background-color'></div><h1>WEATHER APP</h1></div>
+            <Link data-aos-once='true' data-aos="fade-up" to="/Weatherapp">
+                <div onClick={toTop}  className='todo-list'><div className='background-color'></div><h1>WEATHER APP</h1></div>
                 <div  data-aos-once='true' data-aos="fade-up" className='sepLine'></div>
             </Link>
-            <Link to="/Tiktaktoe">
-                <div onClick={toTop} data-aos-once='true' data-aos="fade-up" className='todo-list'><div className='background-color'></div><h1>TIC TAC TOE</h1></div>
+            <Link data-aos-once='true' data-aos="fade-up" to="/Tiktaktoe">
+                <div onClick={toTop}  className='todo-list'><div className='background-color'></div><h1>TIC TAC TOE</h1></div>
                 <div  data-aos-once='true' data-aos="fade-up" className='sepLine'></div>
             </Link>
-            <Link to="/Grades">
-                <div onClick={toTop} data-aos-once='true' data-aos="fade-up" className='todo-list'><div className='background-color'></div><h1>GRADE AVERAGE </h1></div>
+            <Link data-aos-once='true' data-aos="fade-up" to="/Grades">
+                <div onClick={toTop} className='todo-list'><div className='background-color'></div><h1>GRADE AVERAGE </h1></div>
             </Link>
         </div>
     </div>
