f0054736a13ff6652861ceb3d6b2190fb0f39351
sbialynicki <s.bialynicki@interactivesystems.pl>
2021-07-28 19:04:01 +0300

    black jack out1

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -40,9 +40,7 @@ function Projects() {
                 <div onClick={toTop} className='todo-list'><div className='background-color'></div><h1>MEMORY GAME</h1><span>OPEN</span></div>
                 <div  data-aos-once='true' data-aos="fade-up" className='sepLine'></div>
             </Link>
-            {/*<Link data-aos-once='true' data-aos="fade-up" to="/Blackjack">*/}
-            {/*    <div onClick={toTop} className='todo-list'><div className='background-color'></div><h1>BLACKJACK</h1><span>OPEN</span></div>*/}
-            {/*</Link>*/}
+
         </div>
     </div>
 }
