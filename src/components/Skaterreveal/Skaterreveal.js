574f9ba9af5be99d9669782a40a6755966587888
slav <sajsdob@gmail.com>
2023-07-21 22:25:24 +0200

    padding

--- src/components/Skaterreveal/Skaterreveal.js
+++ src/components/Skaterreveal/Skaterreveal.js
@@ -23,16 +23,18 @@ function Skaterreveal() {
 
     return (
         <div data-aos="slide-down" className='app'>
+            <div id='head'>
             <h1>SKATER REVEAL GAME</h1>
             <h2>Check out a game I've created for iOS and Android:</h2>
+            </div>
             <img id='sperm' src={sperm} alt="Sperm" />
             <div id="buttons">
-                 <a class="storeButton" href="https://apps.apple.com/app/skater-reveal/id6451123162" target="_blank">
-                  <img width="200" src={apple} alt="App Store"/>
+                 <a href="https://apps.apple.com/app/skater-reveal/id6451123162" target="_blank">
+                  <img  class="storeButton"  width="200" src={apple} alt="App Store"/>
                  </a>
           
-                 <a  class="storeButton" href="https://play.google.com/store/apps/details?id=com.sajsdob.SkaterReveal" target="_blank">
-                   <img width="200" src={play} alt="Google Play Store"/>
+                 <a href="https://play.google.com/store/apps/details?id=com.sajsdob.SkaterReveal" target="_blank">
+                   <img  class="storeButton"   img width="200" src={play} alt="Google Play Store"/>
                  </a>
              </div>
             <h1 id='price'>It's only $0.99 😉</h1>
